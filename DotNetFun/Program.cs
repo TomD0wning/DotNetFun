7657e6d46538d5b090c6a950f4de69fed38259a8
Tom.Downing <tomp.downing@sainsburys.co.uk>
2018-07-31 14:42:50 +0100

    Add project files.

---
+++ DotNetFun/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace DotNetFun
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
